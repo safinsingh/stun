182e2bb03413f3ee0b528a7f6d22cea7817d7ad5
safinsingh <safin.singh@gmail.com>
2020-10-08 20:43:20 -0700

    feat: implement stunlex encapsulation and organize methods

--- stunlex/src/lib.rs
+++ stunlex/src/lib.rs
@@ -1,49 +1,53 @@
 use std::iter::Iterator;
 mod tests;
 
+impl<'a> Iterator for Lexer<'a> {
+	type Item = Token;
+
+	fn next(&mut self) -> Option<Token> {
+		if let Some(c) = self.current_char() {
+			let next_char = self.peek_char();
+			match c {
+				'\n' => self.newline(),
+				'-' if next_char == Some('-') => self.single_line_comment(),
+				'"' => self.single_line_string(),
+				'=' if next_char == Some('=') => self.equate(),
+				'=' => self.assign(),
+				' ' => self.whitespace(),
+				'A'..='Z' | 'a'..='z' | '_' => self.identifier(),
+				'0'..='9' => self.get_number(),
+				_ => self.undefined(),
+			}
+		} else {
+			None
+		}
+	}
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TokType {
+	Assign,
+	Equate,
+	True,
+	False,
+	Newline,
+	SingleLineString(String),
+	Undefined(String),
+	Ident(String),
+	SingleLineComment(String),
+	Number(f64),
+}
+
 pub struct Lexer<'a> {
-	pub input: &'a str,
-	pub cursor: usize,
-	pub pseudo_cursor: usize,
-	pub line: usize,
+	pub(crate) input: &'a str,
+	pub(crate) cursor: usize,
+	pub(crate) pseudo_cursor: usize,
+	pub(crate) line: usize,
 }
 
 impl<'a> Lexer<'a> {
-	pub fn new(input: &'a str) -> Self {
-		Lexer {
-			input,
-			cursor: 0,
-			pseudo_cursor: 0,
-			line: 1,
-		}
-	}
-
-	// pub fn peek_str(&self, chars: usize) -> Option<&'a str> {
-	// 	if self.input.len() <= self.cursor + chars {
-	// 		None
-	// 	} else {
-	// 		Some(&self.input[self.cursor..self.cursor + chars])
-	// 	}
-	// }
-
-	pub fn peek_char(&self) -> Option<char> {
-		self.get_char(self.cursor + 1)
-	}
-
-	pub fn translate(&mut self, chars: usize) {
-		self.cursor += chars;
-		self.pseudo_cursor += chars;
-	}
-
-	pub fn get_char(&self, num: usize) -> Option<char> {
-		self.input.chars().nth(num)
-	}
-
-	pub fn current_char(&self) -> Option<char> {
-		self.get_char(self.cursor)
-	}
-
-	pub fn get_number(&mut self) -> Option<Token> {
+	// parsing methods
+	pub(crate) fn get_number(&mut self) -> Option<Token> {
 		let mut s = String::new();
 		let start = self.pseudo_cursor;
 
@@ -73,7 +77,7 @@ impl<'a> Lexer<'a> {
 		}
 	}
 
-	pub fn single_line_string(&mut self) -> Option<Token> {
+	pub(crate) fn single_line_string(&mut self) -> Option<Token> {
 		let mut s = String::new();
 		let start = self.pseudo_cursor;
 
@@ -101,7 +105,7 @@ impl<'a> Lexer<'a> {
 		))
 	}
 
-	pub fn identifier(&mut self) -> Option<Token> {
+	pub(crate) fn identifier(&mut self) -> Option<Token> {
 		let mut s = String::new();
 		let start = self.pseudo_cursor;
 
@@ -127,7 +131,7 @@ impl<'a> Lexer<'a> {
 		))
 	}
 
-	pub fn single_line_comment(&mut self) -> Option<Token> {
+	pub(crate) fn single_line_comment(&mut self) -> Option<Token> {
 		let mut s = String::new();
 		let start = self.pseudo_cursor;
 
@@ -148,7 +152,7 @@ impl<'a> Lexer<'a> {
 		))
 	}
 
-	pub fn newline(&mut self) -> Option<Token> {
+	pub(crate) fn newline(&mut self) -> Option<Token> {
 		self.translate(1);
 		self.pseudo_cursor = 0;
 		self.line += 1;
@@ -159,7 +163,7 @@ impl<'a> Lexer<'a> {
 		))
 	}
 
-	pub fn equate(&mut self) -> Option<Token> {
+	pub(crate) fn equate(&mut self) -> Option<Token> {
 		let start = self.pseudo_cursor;
 		self.translate(2);
 
@@ -169,7 +173,7 @@ impl<'a> Lexer<'a> {
 		))
 	}
 
-	pub fn assign(&mut self) -> Option<Token> {
+	pub(crate) fn assign(&mut self) -> Option<Token> {
 		let start = self.pseudo_cursor;
 		self.translate(1);
 
@@ -179,12 +183,12 @@ impl<'a> Lexer<'a> {
 		))
 	}
 
-	pub fn whitespace(&mut self) -> Option<Token> {
+	pub(crate) fn whitespace(&mut self) -> Option<Token> {
 		self.translate(1);
 		self.next()
 	}
 
-	pub fn undefined(&mut self) -> Option<Token> {
+	pub(crate) fn undefined(&mut self) -> Option<Token> {
 		let start = self.pseudo_cursor;
 		self.translate(1);
 
@@ -193,29 +197,32 @@ impl<'a> Lexer<'a> {
 			Span::new(start, self.pseudo_cursor, self.line),
 		))
 	}
-}
 
-impl<'a> Iterator for Lexer<'a> {
-	type Item = Token;
-
-	fn next(&mut self) -> Option<Token> {
-		if let Some(c) = self.current_char() {
-			let next_char = self.peek_char();
-
-			match c {
-				'\n' => self.newline(),
-				'-' if next_char == Some('-') => self.single_line_comment(),
-				'"' => self.single_line_string(),
-				'=' if next_char == Some('=') => self.equate(),
-				'=' => self.assign(),
-				' ' => self.whitespace(),
-				'A'..='Z' | 'a'..='z' | '_' => self.identifier(),
-				'0'..='9' => self.get_number(),
-				_ => self.undefined(),
-			}
-		} else {
-			None
+	// utils
+	pub fn new(input: &'a str) -> Self {
+		Lexer {
+			input,
+			cursor: 0,
+			pseudo_cursor: 0,
+			line: 1,
 		}
+	}
+
+	pub(crate) fn peek_char(&self) -> Option<char> {
+		self.get_char(self.cursor + 1)
+	}
+
+	pub(crate) fn translate(&mut self, chars: usize) {
+		self.cursor += chars;
+		self.pseudo_cursor += chars;
+	}
+
+	pub(crate) fn get_char(&self, num: usize) -> Option<char> {
+		self.input.chars().nth(num)
+	}
+
+	pub(crate) fn current_char(&self) -> Option<char> {
+		self.get_char(self.cursor)
 	}
 }
 
@@ -226,23 +233,9 @@ pub struct Token {
 }
 
 impl Token {
-	pub fn new(kind: TokType, span: Span) -> Self {
+	pub(crate) fn new(kind: TokType, span: Span) -> Self {
 		Token { kind, span }
 	}
-}
-
-#[derive(Debug, PartialEq)]
-pub enum TokType {
-	Assign,
-	Equate,
-	True,
-	False,
-	Newline,
-	SingleLineString(String),
-	Undefined(String),
-	Ident(String),
-	SingleLineComment(String),
-	Number(f64),
 }
 
 #[derive(Debug, PartialEq)]
@@ -253,7 +246,7 @@ pub struct Span {
 }
 
 impl Span {
-	pub fn new(start: usize, end: usize, line: usize) -> Self {
+	pub(crate) fn new(start: usize, end: usize, line: usize) -> Self {
 		Span { start, end, line }
 	}
 }
